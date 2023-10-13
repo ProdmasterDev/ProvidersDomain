b6b8ad8d079727538e78339eab3514d4aa1ae624
Vladimir <mrdrouf777@yandex.ru>
2023-10-13 09:13:53 +0300

    add new models for orders

---
+++ Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvidersDomain.Models
+{
+    public class Order : ITimeStampedModel
+    {
+        [Key]
+        public long Id { get; set; }
+        [JsonProperty("jridn")]
+        public long JrId { get; set; }
+        [JsonProperty("journalidn")]
+        public long? JournalId { get; set; }
+        [JsonProperty("date")]
+        public long? Object { get; set; }
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModified {  get; set; }
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public virtual List<Product> Products { get; set; } = new();
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public virtual List<OrderProductPart> OrderProductPart { get; set; } = new();
+    }
+}
---
+++ Models/OrderProductPart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvidersDomain.Models
+{
+    public class OrderProductPart : ITimeStampedModel
+    {
+        public long ProductId { get; set; }
+        public long OrderId { get; set; }
+        public double Price { get; set; }
+        public virtual Product Product { get; set; }
+        public virtual Order Order { get; set; }
+        public DateTime CreatedAt {  get; set; }
+        public DateTime LastModified {  get; set; }
+    }
+}
